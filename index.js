21c19145a0374ee59d2a0ef053c32b34e9fea751
Yoshihide-Nishimoto <ynishimoto@wbcompany.co.jp>
2017-06-24 12:59:40 +0900

    add express

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 var http = require('http');
 var express = require('express');
+var path = require('path');
 var app = express();
 var bodyParser = require('body-parser');
 app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname,'public')));
 
 //var server = http.createServer(function(request, response) {
 
@@ -19,7 +21,7 @@ var port = process.env.PORT || 1337;
 
 app.get('/', function (req, res) {
   console.log("ここまでOK");
-  res.render('index', { title: 'Hey', message: 'Hello there!'});
+  res.render('index', { title: 'APIサンプル', message:'aasdfsdf'});
 });
 
 app.listen(port,function(){
