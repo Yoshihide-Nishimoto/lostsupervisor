64b35e94bdb1bf08aaeea074f163c316d4f95548
Yoshihide-Nishimoto <ynishimoto@wbcompany.co.jp>
2017-06-24 09:50:57 +0900

    azuregit add .

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
-var http = require('http');
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
 
-var server = http.createServer(function(request, response) {
+//var route_index = require('./routes/index')(express);
 
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello Azure!!");
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+//app.set('port', (process.env.PORT || 3001));
+const PORT = process.env.PORT || 3001
+
+//app.use('/',route_index);
+
+app.get('/', function (req, res) {
+  console.log("ここまでOK");
+  res.render('index', { title: 'Hey', message: 'Hello there!'});
 });
 
-var port = process.env.PORT || 1337;
-server.listen(port);
-
-console.log("Server running at http://localhost:%d", port);
+app.listen(3001,function(){
+    console.log("Running on port 3001 ...")
+});
